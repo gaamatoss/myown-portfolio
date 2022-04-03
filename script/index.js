cf51979bbc5919955176552a90958b96994ed39a
gaamatoss <gsilva505@hotmail.com>
2022-04-03 15:50:50 -0300

    finalizando layout

--- script/index.js
+++ script/index.js
@@ -1,5 +1,10 @@
 const btnMenu = document.querySelector(".menu__button")
 const navMenu = document.querySelector(".menu__nav")
+const sendButton = document.querySelector("#sendButton")
+const sendMessage = document.querySelector(".submit__message")
+const nameInput = document.querySelector("#name")
+const emailInput = document.querySelector("#email")
+const messageInput = document.querySelector("#message")
 
 btnMenu.addEventListener("click", function(){
     navMenu.classList.toggle('menu__mobile') 
